68d51f6ed68ba07883daf4bc8a89e332569e4942
kiellzz <ezequiellsouzaa13@gmail.com>
2026-05-12 01:20:12 -0300

    add Cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from "cors"
 import authRoutes from "./routes/authRoutes"
 import playerRoutes from "./routes/playerRoutes"
 import matchRoutes from "./routes/matchRoutes"
@@ -6,6 +7,7 @@ import statsRoutes from "./routes/statsRoutes"
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use("/api/auth", authRoutes)
